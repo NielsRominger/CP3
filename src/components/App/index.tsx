1a0c2439c22e995a86b7a3e90b0e143c61a9c284
niels <nielsrominger@gmail.com>
2023-04-06 11:41:17 +0200

    on enregistre la valeur à la soumission du formulaire

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -1,9 +1,24 @@
+import { useState, useTransition, ChangeEvent, FormEvent } from 'react';
 import SearchBar from '../SearchBar';
 import Message from '../Message';
 import './styles.scss';
 import ReposResults from '../ReposResults';
 
 function App() {
+  const [formValue, setFormValue] = useState('');
+  const [formSubmittedValue, setFormSubmittedValue] = useState('');
+
+  const handleOnChangeInput = (event: ChangeEvent<HTMLInputElement>) => {
+    setFormValue(event.target.value);
+  };
+
+  const handleSubmitFormValue = (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    setFormSubmittedValue(formValue);
+    setFormValue('');
+    console.log(formSubmittedValue);
+  };
+
   return (
     <div className="app">
       <img
@@ -11,7 +26,11 @@ function App() {
         alt="github logo and title"
       />
 
-      <SearchBar />
+      <SearchBar
+        ValueInSearchBar={formValue}
+        handleOnChangeInput={handleOnChangeInput}
+        handleSubmitFormValue={handleSubmitFormValue}
+      />
       <Message />
       <ReposResults />
     </div>
--- src/components/SearchBar/index.tsx
+++ src/components/SearchBar/index.tsx
@@ -1,12 +1,26 @@
-import React from 'react';
+import { ChangeEvent, FormEvent } from 'react';
 import { Form } from 'semantic-ui-react';
 import './styles.scss';
 
-function SearchBar() {
+interface SearchBarProps {
+  handleOnChangeInput: (event: ChangeEvent<HTMLInputElement>) => void;
+  handleSubmitFormValue: (event: FormEvent<HTMLFormElement>) => void;
+  ValueInSearchBar: string;
+}
+
+function SearchBar({
+  handleOnChangeInput,
+  handleSubmitFormValue,
+  ValueInSearchBar,
+}: SearchBarProps) {
   return (
-    <Form className="search_bar">
+    <Form className="search_bar" onSubmit={handleSubmitFormValue}>
       <Form.Field>
-        <input className="search_field" />
+        <input
+          className="search_field"
+          onChange={handleOnChangeInput}
+          value={ValueInSearchBar}
+        />
       </Form.Field>
     </Form>
   );
